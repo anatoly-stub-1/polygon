35e05cd7cceea678324cf885bfb78d3714c029a1
Anatoly Satanovskiy <polona1s3@gmail.com>
2017-06-05 20:45:18 +0300

    changes in main

--- src/main/java/local/polygon/gitstudy/MyClass.java
+++ src/main/java/local/polygon/gitstudy/MyClass.java
@@ -4,12 +4,8 @@ public class MyClass
 {
 	public static void main(String[] args)
 	{
-		String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " + "Suspendisse in aliquam dui. Sed cursus scelerisque lorem ultricies consectetur. "
-				+ "Interdum et malesuada fames ac ante ipsum primis in faucibus. " + "In imperdiet nibh dui, quis aliquam leo volutpat a. " + "Nam congue sollicitudin cursus. Sed vel mauris risus. "
-				+ "Nulla ut condimentum elit. Vestibulum quis vulputate sem. " + "Donec a ligula laoreet, sagittis neque quis, porttitor arcu. "
-				+ "Fusce porta metus sit amet ex sodales, tristique vulputate elit scelerisque. " + "Etiam suscipit, diam vel rutrum vehicula, sem dui efficitur odio, "
-				+ "eu varius arcu lacus vitae lacus. Curabitur vehicula, neque ac placerat pellentesque, " + "nunc felis mattis orci, vitae rhoncus libero dui interdum nibh. "
-				+ "Curabitur consectetur faucibus lorem et hendrerit.";
-		System.out.println(lorem);
+		TextProvider lorem  = new Lorem();
+		String text = lorem.fetchText();
+		System.out.println(text);
 	}
 }
