80422f34da965a5c78d4b9e372a452ccfade5be7
Anatoly Satanovskiy <sat.anatoliy@gmail.com>
2017-06-05 18:42:19 +0300

    initial commit

---
+++ src/test/java/local/polygon/gitstudy/tests/SimpleTest.java
@@ -0,0 +1,19 @@
+package local.polygon.gitstudy.tests;
+
+import org.junit.Test;
+
+public class SimpleTest
+{
+	@Test
+	public void testOutput()
+	{
+		String lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " + "Suspendisse in aliquam dui. Sed cursus scelerisque lorem ultricies consectetur. "
+				+ "Interdum et malesuada fames ac ante ipsum primis in faucibus. " + "In imperdiet nibh dui, quis aliquam leo volutpat a. " + "Nam congue sollicitudin cursus. Sed vel mauris risus. "
+				+ "Nulla ut condimentum elit. Vestibulum quis vulputate sem. " + "Donec a ligula laoreet, sagittis neque quis, porttitor arcu. "
+				+ "Fusce porta metus sit amet ex sodales, tristique vulputate elit scelerisque. " + "Etiam suscipit, diam vel rutrum vehicula, sem dui efficitur odio, "
+				+ "eu varius arcu lacus vitae lacus. Curabitur vehicula, neque ac placerat pellentesque, " + "nunc felis mattis orci, vitae rhoncus libero dui interdum nibh. "
+				+ "Curabitur consectetur faucibus lorem et hendrerit.";
+		
+		System.out.println(lorem);
+	}
+}
